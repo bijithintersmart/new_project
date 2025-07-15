883bf845ad683cdc301bc36a26586ff271cbbb1f
Bijith InterSmart <bijith@intersmart.in>
2025-07-15 13:33:14 +0530

    app error class added

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import authRoutes from "./src/routes/authRoutes.js";
 import userRoutes from "./src/routes/userRoutes.js";
 import pdfRoutes from "./src/routes/pdfRoutes.js";
 import outSideRoutes from "./src/routes/outSideRoutes.js";
+import AppError from "./src/utils/appError.js";
 import db from "./models/index.js";
-import messages from "./src/utils/constants.js";
 
 //env config
 dotEnv.config();
@@ -33,16 +33,18 @@ app.get("/error", (req, res, next) => {
 });
 
 // 404 handler
-app.use((req, res, next) => {
-  res.status(404).json({ status: "error", message: messages.notFound });
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 // Global error handler
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).json({
-    status: "error",
-    message: err.message || "Internal Server Error",
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
