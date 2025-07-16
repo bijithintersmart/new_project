33834ce0907f283ddf422dc88a3a4c6ea2e52327
Bijith InterSmart <bijith@intersmart.in>
2025-07-16 10:35:38 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -9,12 +9,23 @@ const dbConfig = config[env];
 
 const db = {};
 
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-  host: dbConfig.host,
-  dialect: 'postgres',
-  logging: false,
-});
-
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+      host: dbConfig.host,
+      dialect: "postgres",
+      logging: false,
+    });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
