507239a1e79800c29197e00016d6657bf3a9df5e
Bijith InterSmart <bijith@intersmart.in>
2025-07-16 10:30:13 +0530

    config udpated

--- models/index.js
+++ models/index.js
@@ -1,18 +1,20 @@
 import { Sequelize, DataTypes } from "sequelize";
 import UserModel from "./user.js";
 import config from "../config/config.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const env = process.env.NODE_ENV || "development";
 const dbConfig = config[env];
 
 const db = {};
 
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  dbConfig
-);
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: 'postgres',
+  logging: false,
+});
+
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
